303ec2fbb5e72b58bf7226ca4c7b1ab58e7a2590
sorfgame <zhhf1988@gmail.com>
2020-07-18 16:21:26 +0800

    1213

--- main.3a6fb.js
+++ main.3a6fb.js
@@ -89,12 +89,12 @@ window.boot = function () {
             else if (settings.orientation === 'portrait') {
                 cc.view.setOrientation(cc.macro.ORIENTATION_PORTRAIT);
             }
-            // cc.view.enableAutoFullScreen([
-            //     cc.sys.BROWSER_TYPE_BAIDU,
-            //     cc.sys.BROWSER_TYPE_WECHAT,
-            //     cc.sys.BROWSER_TYPE_MOBILE_QQ,
-            //     cc.sys.BROWSER_TYPE_MIUI,
-            // ].indexOf(cc.sys.browserType) < 0);
+            cc.view.enableAutoFullScreen([
+                cc.sys.BROWSER_TYPE_BAIDU,
+                cc.sys.BROWSER_TYPE_WECHAT,
+                cc.sys.BROWSER_TYPE_MOBILE_QQ,
+                cc.sys.BROWSER_TYPE_MIUI,
+            ].indexOf(cc.sys.browserType) < 0);
         }
 
         // Limit downloading max concurrent task to 2,
@@ -140,7 +140,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.4478e.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.462dd.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
@@ -178,7 +178,7 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.a144a.js');
+        require('src/settings.faab1.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -186,7 +186,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.a144a.js');
+        require('src/settings.faab1.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
